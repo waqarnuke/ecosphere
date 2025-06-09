c95dfebd3438c9c74b318bdb19380b0af00ab250
waqarnuke <wnukedev@gmail.com>
2025-06-09 17:46:07 -0500

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'task1' title`, () => {
+  it(`should have the 'ecosphere' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task1');
+    expect(app.title).toEqual('ecosphere');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, task1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecosphere');
   });
 });
