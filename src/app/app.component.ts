ef4486107ad4c0b8f3a9e2ce7c5d74a7cf2208c5
AnamShaikh <anam.adv.dev@gmail.com>
2025-07-15 02:07:47 +0200

    contactpageupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from './layout/footer/footer.component';
+import { FormsModule } from '@angular/forms'; // ✅ import this
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,FormsModule,
 
   ],
   templateUrl: './app.component.html',
--- src/app/features/contactus/contactus.component.ts
+++ src/app/features/contactus/contactus.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { FormsModule } from '@angular/forms';
+
+
 @Component({
   selector: 'app-contactus',
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
-export class ContactusComponent {
+export class ContactusComponent  {
 
+  sendEmail(e: Event) {
+    e.preventDefault(); // form reload na ho
+
+    emailjs.sendForm(
+      'service_ij63i7a',     // 🔁 EmailJS se copy karo
+      'template_x83un3l',    // 🔁 EmailJS se copy karo
+      e.target as HTMLFormElement,
+      'Wn-AXhwukSinpZ7Sf'      // 🔁 EmailJS account se
+    ).then(() => {
+      alert('✅ Message sent successfully!');
+      (e.target as HTMLFormElement).reset(); // Form reset
+    }).catch((error) => {
+      console.error('❌ Email send failed:', error);
+      alert('Something went wrong. Please try again.');
+    });
+  }
 }
